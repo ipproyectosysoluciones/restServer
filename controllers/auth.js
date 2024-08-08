7e95ced6d8bdf12910433f01798581c258200ea2
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-08-08 18:51:17 -0500

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,57 @@
 import { request, response } from 'express';
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.js';
 
 
-const login = ( req = request, res = response ) => {
+/**
+ * @name login
+ * @description Controlador para manejar las solicitudes POST y iniciar sesión de un usuario en la base de datos.
+ * @param {*} req Objeto de solicitud que contiene la información enviada por el cliente.
+ * @param {*} res Objeto de respuesta que se utiliza para enviar la respuesta de vuelta al cliente.
+ * @returns { Promise<void> } Devuelve una promesa que se resuelve cuando la solicitud se completa.
+ */
+const login = async( req = request, res = response ) => {
 
-  res.json({
-    msg: 'Login ok',
-  });
+  const { email, password } = req.body;
+
+  try {
+    // Check if the email exists.
+    const user = await User.findOne({ email });
+
+    if ( !user ) {
+      return res.status( 400 ).json({ 
+        msg: 'Usuario / Password no son correctos - email' 
+      });
+    };
+
+    // Check if the user is active.
+    if ( !user.state ) {
+      return res.status( 400 ).json({
+        msg: 'Usuario / Password no son correctos - state: false'
+      });
+    };
+
+    // Check if the password is correct.
+    const validPassword = bcryptjs.compareSync( password, user.password );
+
+    if ( !validPassword ) {
+      return res.status( 400 ).json({ 
+        msg: 'Usuario / Password no son correctos - password' 
+      });
+    };
+
+    // Generating an authentication token - JWT.
+    
+    res.json({
+      msg: 'Login ok',
+    });
+  } catch ( error ) {
+    console.log( error );
+    return res.status( 500 ).json({
+      msg: 'Hable con el Adminsitrador',
+    });
+  };
+
 };
 
 export {
