68327c669005b5ab87e932c8e3a62c27d0f5816d
Bladimir G. Parra B. | IP Proyectos y Soluciones <info@ipproyectosysoluciones.com.co>
2024-08-09 03:10:03 -0500

    Validate Google Token - BackEnd

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import { request, response } from 'express';
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.js';
 import { generateJWT } from '../helpers/generate-jwt.js';
+import { googleVerify } from '../helpers/google-verify.js';
 
 
 /**
@@ -57,14 +58,32 @@ const login = async( req = request, res = response ) => {
 
 };
 
-const googleSigIn = ( req = request, res = response ) => {
+/**
+ * @name googleSigIn
+ * @description Controlador para manejar las solicitudes POST y iniciar sesión de un usuario con Google.
+ * @param {*} req Objeto de solicitud que contiene la información enviada por el cliente.
+ * @param {*} res Objeto de respuesta que se utiliza para enviar la respuesta de vuelta al cliente.
+ * @returns { Promise<void> } Devuelve una promesa que se resuelve cuando la solicitud se completa.
+ */
+const googleSigIn = async( req = request, res = response ) => {
 
   const { id_token } = req.body;
 
-  res.json({
-    msg: 'Sign-In Ok!!',
-    id_token,
-  });
+  try {
+    const googleUser = await googleVerify( id_token );
+    console.log( googleUser );
+    
+    res.json({
+      msg: 'Sign-In Ok!!',
+      id_token,
+    });
+  } catch ( error ) {
+    console.log( error );
+    return res.status( 401 ).json({ 
+      ok: false,
+      msg: 'El Token no se pudo verificar - Google' 
+    });
+  };
 };
 
 export {
---
+++ helpers/google-verify.js
@@ -0,0 +1,29 @@
+import { OAuth2Client } from 'google-auth-library';
+
+const client = new OAuth2Client();
+
+/**
+ * @name googleVerify
+ * @description Verifica un token de Google y devuelve la información del usuario.
+ * @param { string } token - Token de Google a verificar.
+ * @returns { Promise<Object> } Devuelve la información del usuario verificada.
+ */
+async function googleVerify( token = '' ) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_ID,  
+      // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const { email, name, picture } = ticket.getPayload();
+
+  return{
+    email,
+    name,
+    img: picture,
+  };
+}
+// verify().catch( console.error );
+
+export { googleVerify };
